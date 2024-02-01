bb9853111f32eb79fb38fb17c3644e449c6567e5
Alireza Moradi <me.alirezamoradi@gmail.com>
2024-02-01 19:50:17 +0330

    add animation to someInfo section values

--- 1/assets/scripts/app.js
+++ 1/assets/scripts/app.js
@@ -1,9 +1,25 @@
 // Elements
 const upBtn = document.querySelector('#top');
+const counters = document.querySelectorAll('.count');
+const speed = 4000;
 
 // Functions
-const goUp = () => {
+const starterCount = () => {
+  counters.forEach((value) => {
+      let startValue = 0;
+      let endValue = parseInt(value.getAttribute('data-target'));
+      let duration = Math.trunc(speed / endValue);
+      let counter = setInterval(() => {
+        startValue++;
+        value.textContent = startValue;
+        if (startValue === endValue) {
+          clearInterval(counter);
+        }
+      }, duration);
+    });
+};
 
+const goUp = () => {
   scrollTo({top: 0, behavior: 'smooth'});
 };
 
@@ -19,5 +35,6 @@ const goUpDisplay = () => {
 };
 
 // Events
+window.addEventListener('scroll', starterCount, { once: true });
 window.addEventListener('scroll', goUpDisplay);
 upBtn.addEventListener('click', goUp);
