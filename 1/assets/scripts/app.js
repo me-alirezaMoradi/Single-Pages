1b1b37364f5f72a84e98344ea49e25588e45195e
Alireza Moradi <me.alirezamoradi@gmail.com>
2024-02-01 18:36:07 +0330

    update back to top btn

--- 1/assets/scripts/app.js
+++ 1/assets/scripts/app.js
@@ -3,8 +3,21 @@ const upBtn = document.querySelector('#top');
 
 // Functions
 const goUp = () => {
+
   scrollTo({top: 0, behavior: 'smooth'});
 };
 
+const goUpDisplay = () => {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    upBtn.style.display = 'flex';
+  } else {
+    upBtn.style.display = 'none';
+  }
+};
+
 // Events
+window.addEventListener('scroll', goUpDisplay);
 upBtn.addEventListener('click', goUp);
